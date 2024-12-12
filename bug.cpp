1cd0aaaeba0f1a9cc6df58fc23c2d737136c07de
BugHunter-X <askzenai@gmail.com>
2024-12-12 01:24:06 +0100

    Add bug.cpp file

---
+++ bug.cpp
@@ -0,0 +1,5 @@
+std::vector<bool> vec;
+vec.push_back(true);
+vec.push_back(false);
+bool b = vec[1]; // Accessing element at index 1
+bool c = vec[2]; // Accessing element at index 2 (out of bounds)
